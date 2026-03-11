217b901f0956d056b2bd4817736c15a2aca52aed
Kirti Vohra <kirtivohra55@gmail.com>
2026-03-11 14:21:00 +0530

    changes

--- ledger_system/settings.py
+++ ledger_system/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4=4)@od$iw1)#@iagwe)z=-p8aqn1=(-g@yh!%uxfwvd1rh$of'
+# SECRET_KEY = 'django-insecure-4=4)@od$iw1)#@iagwe)z=-p8aqn1=(-g@yh!%uxfwvd1rh$of'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
